b72d050c79878f58c20cbbe2e40a09279050b48f
rjkush17 <rishabhkushwahrj@gmail.com>
2024-09-15 20:44:36 +0530

    devise gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
+  root 'home#index'        # Sets the root path to the 'index' action in HomeController
+  devise_for :users        # Generates routes for user authentication with Devise
 end
+

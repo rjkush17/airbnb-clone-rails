003afd3817f45a278e5853ff20cc76436920a7b7
rjkush17 <rishabhkushwahrj@gmail.com>
2024-09-16 08:25:06 +0530

    learn simulues and add el transistion

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
+
+export default class extends Controller {
+
+  static targets =['dropdown', 'openUserMenu']
+
+  connect () {
+    this.openUserMenuTarget.addEventListener("click", (e)=> {
+      openDropdown(this.dropdownTarget)
+    })
+  }
+}
+
+function openDropdown(element) {
+  toggle(element).then(() => {
+    console.log("Enter transition complete")
+  })
+}
+
+function closeDropdown() {
+  leave(this.dropdownTarget).then(() => {
+    console.log("element destroy")
+      element.destroy();
+  })
+}
